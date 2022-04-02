d8cd4b68cada504720bb9418824ca864636b64fd
omar saade <omarsaadeh11@gmail.com>
2022-04-03 01:16:48 +0300

    Diapp

--- script.js
+++ script.js
@@ -2,6 +2,9 @@ const wrapper = document.querySelector(".wrapper");//wrapper
 const searchInput = wrapper.querySelector("input");
 const synonyms = wrapper.querySelector(".synonyms .list");
 const infoText = wrapper.querySelector(".info-text");
+const volumeIcon = wrapper.querySelector(".word i");
+const removeIcon = wrapper.querySelector(".search span");
+var audio;
 
 
 window.onload = searchInput.focus();
@@ -25,13 +28,17 @@ function data(result, word) {
         document.querySelector(".word span").innerText = phonetics;//adjective/hapi
         document.querySelector(".meaning span").innerText = definitions.definition;//Having a feeling 
         document.querySelector(".example span").innerText = definitions.example;//example
+        audio = new Audio(result[0].phonetics[0].audio);//creating new audio obj and passing audio src
+
+        console.log(audio);
+
         if (definitions.synonyms[0] == "") { //if there is no synonym then hide the synonyms div
             synonyms.parentElement.style.display = "none";
         } else {
             synonyms.parentElement.style.display = "block";
             synonyms.innerHTML = "";
             for (let i = 0; i < 5; i++) {//getting only 5 synonyms out of many
-                let tag = `<span>${lastElement.synonyms[i]},</span>`;//synonyms
+                let tag = `<span onclick=search('${lastElement.synonyms[i]}')>${lastElement.synonyms[i]},</span>`;//synonyms
                 synonyms.insertAdjacentHTML("beforeend", tag);//passing all 5 synonyms inside synonyms div
                 // element.insertAdjacentHTML(position, text);
                 //yaane hetele el tag 2abel teskiret el taggg
@@ -41,6 +48,22 @@ function data(result, word) {
 
     }
 }
+
+
+
+//seach synonyms function
+function search(word) {
+
+    searchInput.value = word;
+    fetchApi(word);
+}
+
+
+
+
+
+
+
 
 
 
@@ -67,6 +90,7 @@ function data(result, word) {
 // https://dictionaryapi.dev/
 //fetch api function
 function fetchApi(word) {
+    wrapper.classList.remove("active");
     infoText.style.color = "#000";
     infoText.innerHTML = `Searching the meaning of <span>"${word}"</span>`;
     let url = `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`;
@@ -87,3 +111,20 @@ searchInput.addEventListener("keyup", e => {
         fetchApi(e.target.value);
     }
 })
+
+
+
+volumeIcon.addEventListener("click", () => {
+    audio.play();
+})
+
+
+
+removeIcon.addEventListener("click", () => {
+    searchInput.value = "";
+    searchInput.focus();
+    wrapper.classList.remove("active");
+    infoText.style.color = "9a9a9a";
+    infoText.innerHTML = "Type any existing word and press enter to get meaning, example, synonyms, etc.";
+
+});
